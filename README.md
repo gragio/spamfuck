a5a84f7d7700c3c4276dcc282f91a7a62f79b38e
Giorgio Grasso <giorgiograsso.90@gmail.com>
2018-09-09 00:45:56 +0200

    Input email and section ID

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # spamfuck
-Script to remove your email from uglyposture.strem only mail list
+Unsubscriber script for remove your email address from uglyposture.strem only mail list
 
 ##Getting Started
+
+Install
 
 ```
 $ composer install
 ```
 
-open `uglyposture.php` and set your email and session
+and run
 ```
-$email = 'example@email.com';
-$phpSession = 'PHPSESSID=yoursession';
+$ php uglyposture.php
 ```
+
+If the response is `Some Thing Wrong` probably you need a session ID.
+
+##Session ID
 For get a session id:
 * Open a new browser tab and visit http://www.uglyposture.stream/
 * Open you broswer ispector and go in Network tab
 * Select `www.uglyposture.stream` request
 * Get `Cookie` value in `Request Headers` section
-
-Run
-```
-$ php uglyposture.php
-```
--- uglyposture.php
+++ uglyposture.php
@@ -1,42 +1,61 @@
 <?php
-
 require __DIR__ . '/vendor/autoload.php';
 
 use GuzzleHttp\Client;
 
-$email = 'example@email.com';
-$phpSession = 'PHPSESSID=yoursession';
+const SESSIONID = 'PHPSESSID=rh8222p5dtinfcltd7qi2tgd53';
+
+$stdin = fopen('php://stdin', 'r');
+
+while (empty($email))  {
+    echo 'Input your email address: ';
+    $email = trim(fgets($stdin));
+}
+
+while (empty($phpSession))  {
+    echo 'Input your email session id [\''.SESSIONID.'\']: ';
+    $phpSession = trim(fgets($stdin)) ?: SESSIONID;
+}
 
 $host = 'www.uglyposture.stream';
 $base_url = 'http://'.$host;
 $client = new Client(['cookies' => true]);
+
+$clientOptions = [
+    'headers' => [
+        'Host' => $host,
+        'Origin' => $base_url,
+        'Pragma' => 'no-cache',
+        'Upgrade-Insecure-Requests' => 1,
+        'Cookie' => $phpSession,
+    ],
+    'form_params' => [
+        'mail' => $email,
+        'save' => 'Unsubscribe',
+    ]
+];
 
 $alphabet = range('A', 'Z');
 for ($i = 0, $l = count($alphabet); $i < $l; ++$i) {
     for ($n = 1; $n < 1000; ++$n) {
         $url = $base_url.'/unsub.php?c='.$n.$alphabet[$i];
         echo $url.PHP_EOL;
-        $result = $client->request('POST', $url, [
-                'headers' => [
-                    'Host' => $host,
-                    'Origin' => $base_url,
-                    'Referer' => $url,
-                    'Pragma' => 'no-cache',
-                    'Upgrade-Insecure-Requests' => 1,
-                    'Cookie' => $phpSession,
-                ],
-                'form_params' => [
-                    'mail' => $email,
-                    'save' => 'Unsubscribe',
-                ]
-            ]);
 
-        preg_match_all('|<h[^>]+>(.*)</h[^>]+>|iU', $result->getBody()->getContents(), $matches);
-        if(isset($matches[1][1])) {
-            echo $matches[1][1].PHP_EOL;
+        $clientOptions['headers']['Referer'] = $url;
+        $result = $client->request('POST', $url, $clientOptions);
+
+        if($response = getHtmlResponse($result->getBody()->getContents())) {
+            echo 'Response: '.$response.PHP_EOL;
             continue;
         }
 
         var_dump($result->getBody()->getContents());
     }
+}
+
+function getHtmlResponse(string $html): ?string
+{
+    preg_match_all('|<h[^>]+>(.*)</h[^>]+>|iU', $html, $matches);
+
+    return $matches[1][1] ?? null;
 }
